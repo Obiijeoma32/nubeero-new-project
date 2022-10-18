d7a8eb370d5689df539fe48ddffbb6d85a0ea78e
Candy Obi <obiijeoma32@gmail.com>
2022-10-18 19:38:32 +0100

    update profile

--- rider.js
+++ rider.js
@@ -1,12 +1,12 @@
 const admin = document.querySelector(".dashboard-admin"); //admin bar
-const viewProfile = document.querySelectorAll(".view-user-profile"); //admin bar
-const createAdmin = document.querySelector(".main-modal-form");
+const viewProfile = document.querySelectorAll(".view-user-profile"); // rider table
+const createAdmin = document.querySelector(".main-modal-form"); // admin form
+const userProfile = document.querySelector(".user-profile"); // modal with the image
 // console.log(admin, viewProfile, createAdmin);
 admin.addEventListener("click", adminHandler);
 for (let i = 0; i < viewProfile.length; i++) {
   viewProfile[i].addEventListener("click", viewProfileHandler);
 
-  const userProfile = document.querySelector(".user-profile");
   function viewProfileHandler() {
     if (userProfile) {
       userProfile.style.display = "block";
